396acce70fbb424d9e8237830dcef5425c8f4aad
bingenk <s10244655@connect.np.edu.sg>
2023-05-11 13:29:30 +0800

    Toast Message and ImageView update

--- app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -22,11 +23,17 @@ public class MainActivity extends AppCompatActivity {
                 if (isFollowing) {
                     followButton.setText("FOLLOW");
                     isFollowing = false;
+                    showToast("Unfollowed");
                 } else {
                     followButton.setText("UNFOLLOW");
                     isFollowing = true;
+                    showToast("Followed");
                 }
             }
         });
     }
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
 }
